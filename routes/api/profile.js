f150620202baf69c1ed576affaf5594d2c5d4149
Tran Bin <binditto@MBP-cua-Tran.hsd1.ca.comcast.net>
2020-02-24 03:00:55 -0800

    Put functionality for profile 'experience'

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,4 +157,88 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+/**
+ * @route   DELETE api/profile
+ * @desc    Delete profile, user & posts
+ * @access  Private
+ */
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route   PUT api/profile/experience
+ * @desc    Add profile experience
+ * @access  Private
+ */
+router.put(
+  '/experience',
+  [
+    auth,
+    check('title', 'Title is required')
+      .not()
+      .isEmpty(),
+    check('company', 'Company is required')
+      .not()
+      .isEmpty(),
+    check('from', 'From Date is required')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    // deal with mongodb
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      // push to beginning of array
+      profile.experience.unshift(newExp);
+      console.log('New experience', profile.experience);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
